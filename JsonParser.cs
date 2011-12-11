bfd843e20646d0f8e3a8b833678c99eae4fb7d34
Michael Ganss <michael@ganss.org>
2011-12-11 22:50:30 +0100

    make a few properties private add xml comments add license

--- JsonParser.cs
+++ JsonParser.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿// 
+// JsonParser.cs
+//  
+// Author:
+//       Michael Ganss <michael@ganss.org>
+// 
+// Copyright (c) 2011 Michael Ganss
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Runtime.Serialization;
@@ -10,8 +36,8 @@ namespace MicroJson
     [Serializable]
     public class ParserException : Exception
     {
-        public int Line { get; set; }
-        public int Column { get; set; }
+        public int Line { get; private set; }
+        public int Column { get; private set; }
 
         public ParserException(string msg, int line, int col)
             : base(msg)
@@ -43,15 +69,61 @@ namespace MicroJson
         }
     }
 
+    /// <summary>
+    /// Parses JSON into POCOs.
+    /// </summary>
     public class JsonParser
     {
-        public string Input { get; private set; }
-        public int InputLength { get; private set; }
-        public int Pos { get; private set; }
-        public int Line { get; private set; }
-        public int Col { get; private set; }
+        string Input { get; set; }
+        int InputLength { get; set; }
+        int Pos { get; set; }
+        int Line { get; set; }
+        int Col { get; set; }
+
+        /// <summary>
+        /// Gets or sets the logger.
+        /// </summary>
+        /// <value>
+        /// The logger.
+        /// </value>
         public ILogger Logger { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to collect line info during parsing.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if line info should be collected during parsing; otherwise, <c>false</c>.
+        /// </value>
         public bool CollectLineInfo { get; set; }
+
+        /// <summary>
+        /// Parse the specified JSON text.
+        /// </summary>
+        /// <param name='text'>
+        /// The JSON text to parse.
+        /// </param>
+        public object Parse(string text)
+        {
+            if (text == null) {
+                ThrowParserException("input is null");
+                return null;
+            }
+
+            Input = text;
+            InputLength = text.Length;
+            Pos = 0;
+            Line = 1;
+            Col = 1;
+
+            var o = Value();
+
+            SkipWhitespace();
+
+            if (Pos != InputLength)
+                ThrowParserException("extra characters at end");
+
+            return o;
+        }
 
         private void WriteLineLog(string msg, params object[] args)
         {
@@ -130,29 +202,6 @@ namespace MicroJson
             AdvanceInput(1);
 
             return c;
-        }
-
-        public object Parse(string text)
-        {
-            if (text == null)
-            {
-                ThrowParserException("input is null");
-                return null;
-            }
-
-            Input = text;
-            InputLength = text.Length;
-            Pos = 0;
-            Line = 1;
-            Col = 1;
-            
-            var o = Value();
-
-            SkipWhitespace();
-
-            if (Pos != InputLength) ThrowParserException("extra characters at end");
-
-            return o;
         }
 
         private object Value()
